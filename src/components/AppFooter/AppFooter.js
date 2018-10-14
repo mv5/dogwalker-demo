6cccc06619487c4cfa787ef926ffbeacac9ea5cc
menashe <menash88@gmail.com>
2018-10-14 15:01:16 +0300

    added walker details footer

---
+++ src/components/AppFooter/AppFooter.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Footer} from '../../styles/styles'
+
+const AppFooter = ({showDetails, hoveredUser}) => (
+    showDetails && hoveredUser.type && hoveredUser.type === "walker" &&
+    <Footer>
+        <p>{hoveredUser.name || hoveredUser.displayName || ""}</p>
+        <p>{hoveredUser.about || ""}</p>
+        <p>{hoveredUser.phone || ""}</p>
+    </Footer>
+)
+
+export default AppFooter
