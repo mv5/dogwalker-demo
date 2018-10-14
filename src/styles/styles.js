b56d43625f135ffd6df46d9132e335719c5d5889
menashe <menash88@gmail.com>
2018-10-14 19:26:57 +0300

    added loader

--- src/styles/styles.js
+++ src/styles/styles.js
@@ -2,26 +2,12 @@ import styled from "styled-components";
 
 import dogsBG from '../assets/dogs-bg.jpg'
 
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Input from '@material-ui/core/Input';
-import MenuItem from '@material-ui/core/MenuItem';
-import FormControl from '@material-ui/core/FormControl';
-import InputLabel from '@material-ui/core/InputLabel';
-import Select from '@material-ui/core/Select';
-import Dialog from '@material-ui/core/Dialog';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import Button from '@material-ui/core/Button';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import Tooltip from '@material-ui/core/Tooltip';
-import AppBar from '@material-ui/core/AppBar';
-import Typography from '@material-ui/core/Typography';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import FormLabel from '@material-ui/core/FormLabel';
-import FormGroup from '@material-ui/core/FormGroup';
+import {
+    AppBar, Card, Dialog, DialogContent, DialogContentText, DialogTitle,
+    Button, Select, Input, InputLabel, MenuItem, FormControl,
+    CardContent, Typography, Tooltip, CssBaseline,
+    FormControlLabel, FormLabel, Checkbox, FormGroup
+} from '@material-ui/core'
 
 export {
     Dialog, DialogContent, DialogContentText, DialogTitle,
