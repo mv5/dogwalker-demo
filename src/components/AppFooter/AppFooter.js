14d91e5283a72b22e03e963a0dd66c87c5aeb59f
menashe <menash88@gmail.com>
2018-10-16 14:59:09 +0300

    fixed footer text

--- src/components/AppFooter/AppFooter.js
+++ src/components/AppFooter/AppFooter.js
@@ -1,16 +1,26 @@
 import React from 'react'
 import { Footer, Typography } from '../../styles/styles'
+import * as userTypes from '../../constants/UserTypes'
 
 const AppFooter = ({ showDetails, hoveredUser }) => (
     <Footer>
-        {showDetails && hoveredUser.type && hoveredUser.type === "walker" ?
+        {showDetails && 
+        (hoveredUser.type === userTypes.WALKER || hoveredUser.type === userTypes.SITTER) ?
             <div>
-                <Typography variant="subtitle1" color="primary">{hoveredUser.name || hoveredUser.displayName || ""}</Typography>
-                <Typography variant="subtitle1" color="primary">{hoveredUser.about || ""}</Typography>
-                <Typography variant="subtitle2" color="primary">{hoveredUser.phone || ""}</Typography>
+                <Typography variant="subtitle1" color="primary">
+                    {hoveredUser.name || hoveredUser.displayName || ""}
+                </Typography>
+                <Typography variant="subtitle1" color="primary">
+                    {hoveredUser.about || ""}
+                </Typography>
+                <Typography variant="subtitle2" color="primary">
+                    {hoveredUser.phone || ""}
+                </Typography>
             </div>
             :
-            <Typography variant="subtitle1" color="secondary">*Hover over a Walker to see his/her details</Typography>
+            <Typography variant="subtitle1" color="secondary">
+                *Hover over a Walker or a Sitter to see his/her details
+            </Typography>
         }
     </Footer>
 )
